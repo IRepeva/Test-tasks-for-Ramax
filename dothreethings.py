40a94767a14b80b38ca473c956bd5eb5e78fe54d
IRepeva <irepeva@gmail.com>
2019-11-28 22:34:41 +0300

    last task added

--- dothreethings.py
+++ dothreethings.py
@@ -1,12 +1,27 @@
+import threading
+
+
 class Doer:
+
+    def __init__(self):
+        self.lock1 = threading.Lock()
+        self.lock2 = threading.Lock()
+        self.lock3 = threading.Lock()
+
+        self.lock2.acquire()
+        self.lock3.acquire()
+
     def first(self, action):
-        # Следующую строчку не убирать
+        self.lock1.acquire()
         action()
-    
+        self.lock2.release()
+
     def second(self, action):
-        # Следующую строчку не убирать
+        self.lock2.acquire()
         action()
-    
+        self.lock3.release()
+
     def third(self, action):
-        # Следующую строчку не убирать
+        self.lock3.acquire()
         action()
+        self.lock1.release()
